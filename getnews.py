461f52e56ec1611e4c6a0da838efe6aaf931e6ff
VitalyNZ <vitaly@borisov.nz>
2023-04-04 12:34:18 +1200

    Playing around

--- getnews.py
+++ getnews.py
@@ -1,38 +1,30 @@
-from newsapi import NewsApiClient
+import requests
 
-#exit(0)
+api_key='eda4bf5358654cafbee74d38c6c001f8'
+country = None #'au' #'au'
+q = "Crypto" #'Tesla'
 
+baseurl = 'https://newsapi.org/v2/top-headlines?'
+#baseurl = 'https://newsapi.org/v2/everything?'
+requesturl = baseurl+'apiKey='+api_key + ('&country='+country if country is not None else "")+ ('&q='+q if q is not None else "")
+response = requests.get(requesturl)
 
-# Init
-newsapi = NewsApiClient(api_key='eda4bf5358654cafbee74d38c6c001f8')
-
-sources = newsapi.get_sources(country='nz')
-print(sources)
-exit(0)
-
-# /v2/top-headlines
-top_headlines = newsapi.get_top_headlines(country='nz', sources='stuff') #q='tesla', 
-
-print(top_headlines)
-
-#news-com-au, abc-news-au
-
-exit(0)
-
-# /v2/everything
-all_articles = newsapi.get_everything(q='bitcoin',
-                                      sources='bbc-news,the-verge',
-                                      domains='bbc.co.uk,techcrunch.com',
-                                      from_param='2017-12-01',
-                                      to='2017-12-12',
-                                      language='en',
-                                      sort_by='relevancy',
-                                      page=2)
-
-print(all_articles)
-
-# /v2/top-headlines/sources
-sources = newsapi.get_sources()
-
-print(sources)
-
+if response.status_code == 200:
+    result = response.json()
+    #print(result)
+    authors=[]
+    url = []
+    for article in result['articles']:
+        #if article['author'] == 'New Zealand Herald':
+        #if article['author'] != 'New Zealand Herald':
+        url.append(article['url'])
+        #authors.append(article['author'])
+    #authors = sorted(set(authors))
+    #for author in authors:
+    #    print(author)
+    url = sorted(set(url))
+    print('Could you please provide a summary (bullet points) on the news (URLs below):')
+    for u in url:
+        print(u)
+else:
+    print(f"Error: {response.status_code}")

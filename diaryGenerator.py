21d86568bc9c2f282130d857ea6265b1d06ca601
VitalyNZ <vitaly@borisov.nz>
2023-04-13 22:38:10 +1200

    Beautifying the code

--- diaryGenerator.py
+++ diaryGenerator.py
@@ -1,101 +1,65 @@
-from datetime import datetime, timedelta
-import requests,os,openai
-
-#list of subjects/questions
-#current utc date -1 as a "from"
-#call news api for each one news in the subject
-#read content and convert to a bullet point with URL at the end
-
-topics = ["Australia","Queensland","New%20Zealand","Aviation"] #,"Auto","Games","Economy","Crypto"]
-
-###################################
-
-articleslimit = 2 #number of article(s) per subject
-sortBy = "publishedAt" #fresh news #"publishedAt" #"relevancy" #"popularity"
-
-nfrom = (datetime.utcnow() - timedelta(days=1)).strftime('%Y-%m-%d') #UTC -1 day to cover all 24hour news
-
-newsApiKey = os.getenv("NEWSAPIKEY")
-
-#collect articles
-articles = {}
-for topic in topics:
-    requesturl = 'https://newsapi.org/v2/everything?'
-    requesturl +='apiKey='+newsApiKey 
-    requesturl +='&language=en'
-    requesturl +="&sortBy="+sortBy
-    requesturl +='&from='+nfrom
-    requesturl +='&pageSize=10'
+#--------------------------------#
+def getNewsArticles(topic, articleslimit = 2, sortBy = "publishedAt"):
+    from datetime import datetime, timedelta
+    import os,requests
+    nfrom = (datetime.utcnow() - timedelta(days=1)).strftime('%Y-%m-%d') #UTC -1 day to cover all 24hour news
+    newsApiKey = os.getenv("NEWSAPIKEY")
+    requesturl = 'https://newsapi.org/v2/everything?'+'apiKey='+newsApiKey+'&language=en'+"&sortBy="+sortBy+'&from='+nfrom+f'&pageSize={articleslimit}'
     requesturl +='&q='+topic
 
-    #get topics and skip existing, pick up only by limits
-
-    #print(requesturl) ##########
-    articles[topic] = []
+    articles = []
     response = requests.get(requesturl)
     if response.status_code == 200:
         result = response.json()
-        limit = 0
         for arr in result['articles']:
-            if limit>=articleslimit:
-                break
-            
             content = {'source': arr['source']['name'],'author':arr['author'], 'title':arr['title'], 'url':arr['url'], 'description':arr['description']}
-            ##urlToImage #publishedAt
-            
-            #just to pick up all with dups
-            #limit+=1
-            #articles[topic].append(content)
-
-            alreadyexists = 0
-            for topic, news in articles.items():
-                for title in news:
-                    if content['title'] in title['title']:
-                        alreadyexists = 1
-                        break
-            if alreadyexists==0:
-                limit+=1
-                articles[topic].append(content) #(arr)
+            articles.append(content)
     else:
         print(f"Error: {response.status_code}")
+    return articles
+#--------------------------------#
 
+#--------------------------------#
+def getArticleSummary(content):
+    import os,openai
+    openai.api_key = os.getenv("OPENAPIKEY")
+    openai.organization = os.getenv("OPENAPIORG")
+    result = """Summarise text below into a short story-line with a two-three emoji (assisting describing content, but not replacing the content):
+    """ + content[:1024]
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo", messages=[{"role": "user", "content": result}], temperature=0.25
+    )
+    return completion.choices[0].message['content']
+#--------------------------------#
 
-#for topic, news in articles.items():
-#    print(topic.replace('%20',' ')+":")
-#    for artice in news:
-#        print(artice['title'])
+#--------------------------------#
+def sendTelegramMessage(message, debug = 1):
+    import os,requests
+    if debug == 0:
+        chat_id = "-1001783307848" #CovetorNews
+        telegramToken = os.getenv("TELEGRAMBOTKEY")
+    else:
+        chat_id = "-1001371860541" #TestTelegramChannel
+        telegramToken = os.getenv("TELEGRAMBOTKEYTEST")
+    
+    requesturl = "https://api.telegram.org/bot" + telegramToken + "/sendMessage" + "?chat_id=" + chat_id + "&parse_mode=HTML" + "&text=" + message 
+    requesturl +="&disable_web_page_preview=true"
+    return requests.get(requesturl)
+#--------------------------------#
 
+## Begin #################################
 
-openai.api_key = os.getenv("OPENAPIKEY")
-openai.organization = os.getenv("OPENAPIORG")
+topics = ["Australia","Queensland"] #,"New%20Zealand","Aviation"] #,"Auto","Games","Economy","Crypto"]
 
-processedArticles = {}
-for topic, news in articles.items():
-    processedArticles[topic] = []
+result = ""
+for topic in topics:
+    news = getNewsArticles(topic)
+    result += "\n<b>" + topic + ":</b>\n"
     for article in news:
-        content = """Summarise text below into a short story-line with a two-three emoji (assisting describing content, but not replacing the content):
-        """ + article['description'][:1024]
-        completion = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo", messages=[{"role": "user", "content": content}], temperature=0.25
-        )
-        processedArticles[topic].append(completion.choices[0].message['content']+" <i>(" + article['source'] + ")</i>")
+        result += getArticleSummary(article['description'])
+        result +=' <i>(<a href="'+article['url']+'">'+article['source']+'</a>)</i>\n'
 
-message = ""
-for topic, content in processedArticles.items():
-    message += "\n<b>"+topic.replace('%20',' ')+":</b>\n"
-    for artice in content:
-        message += artice + "\n"
+response = sendTelegramMessage(result)
+print(response)
 
-#print(message)
-
-telegramToken = os.getenv("TELEGRAMBOTKEY")
-chat_id = "-1001783307848" #CovetorNews
-
-requesturl = "https://api.telegram.org/bot" + telegramToken + "/sendMessage" + "?chat_id=" + chat_id + "&parse_mode=HTML" + "&text=" + message 
-response = requests.get(requesturl)
-if response.status_code == 200:
-    print("Sent to Telegram channel")
-else:
-    print(f"Error: {response.status_code}")
-
-
+## End ##################################
--- telegramCall.py
+++ telegramCall.py
@@ -1,16 +1,61 @@
-import os
-theToken = os.getenv("TELEGRAMBOTKEY")
+import os, requests, json
 
+message = 'Test message again'
 
-print(theToken)
+#telegramToken = os.getenv("TELEGRAMBOTKEY")
+#chat_id = "-1001783307848" #CovetorNews
+
+telegramToken = os.getenv("TELEGRAMBOTKEYTEST")
+chat_id = "-1001371860541" #TestTelegramChannel
+
+requesturl = "https://api.telegram.org/bot" + telegramToken + "/sendMessage" + "?chat_id=" + chat_id + "&parse_mode=HTML" + "&text=" + message 
+response = requests.get(requesturl)
+if response.status_code == 200:
+    print("Sent to Telegram channel")
+else:
+    print(response)
+
 
 
 exit(0)
 
+theToken = os.getenv("TELEGRAMBOTKEY")
+chat_id = "-1001783307848" #CovetorNews
+poll_id = 0
+
+#first get poll_id
+api_url = "https://api.telegram.org/bot" + theToken + "/"
+response = requests.post(api_url + "getUpdates", data={"chat_id": chat_id, "limit": 5})
+result = response.json()
+updates = sorted(result["result"], key=lambda x: x["channel_post"]["date"], reverse=True) #sort by date descending
+for pst in updates:
+    if pst['channel_post']['poll']:
+        poll_id = pst['channel_post']['poll']['id']
+        pollOptions = pst['channel_post']['poll']['options']
+        break
+
+print(poll_id)
+print(pollOptions)
+
+exit(0)
+
+if poll_id!=0:
+    response = requests.post(api_url + "getPoll", data={"chat_id": chat_id,"msg_id": poll_id})
+    result = response.json()
+    print(result)
+
+
+#data = json.loads(response.text)
+#print(data)
+
+
+
+
+
+exit(0)
+
+#post a message
 text = "Testing news reporter bot"
-
-import requests
-
 token = theToken
 chat_id = "-1001371860541" #"OnewheelNZ"
 chat_id = "-1001783307848" #CovetorNews

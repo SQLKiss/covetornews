f4b5886a07d0ab5f35c1d10e36e37b5ad32d6402
VitalyNZ <vitaly@borisov.nz>
2023-04-18 10:16:43 +1200

    Added new news source. Imporved Telegram messaging. Added error handling

--- funcLib.py
+++ funcLib.py
@@ -1,5 +1,13 @@
 #--------------------------------#
-def convertNewsResponseToContent(response):
+def getNewsArticles(topic, articleslimit = 2, sortBy = "publishedAt"):
+    from datetime import datetime, timedelta
+    import os,requests
+    nfrom = (datetime.utcnow() - timedelta(days=1)).strftime('%Y-%m-%d') #UTC -1 day to cover all 24hour news
+    newsApiKey = os.getenv("NEWSAPIKEY")
+    requesturl = 'https://newsapi.org/v2/everything?'+'apiKey='+newsApiKey+'&language=en'+"&sortBy="+sortBy+'&from='+nfrom+f'&pageSize={articleslimit}'
+    requesturl +='&q='+topic
+
+    response = requests.get(requesturl)
     articles = []
     if response.status_code == 200:
         result = response.json()
@@ -10,16 +18,41 @@ def convertNewsResponseToContent(response):
         print(f"Error: {response.status_code}")
     return articles
 
-def getNewsArticles(topic, articleslimit = 2, sortBy = "publishedAt"):
+def getNewsIOArticles(q, articleslimit = 2, category = 'top'):
+    #category = "business,entertainment" #,environment,food,health,politics,science,sports,technology,top,tourism,world
     from datetime import datetime, timedelta
     import os,requests
     nfrom = (datetime.utcnow() - timedelta(days=1)).strftime('%Y-%m-%d') #UTC -1 day to cover all 24hour news
-    newsApiKey = os.getenv("NEWSAPIKEY")
-    requesturl = 'https://newsapi.org/v2/everything?'+'apiKey='+newsApiKey+'&language=en'+"&sortBy="+sortBy+'&from='+nfrom+f'&pageSize={articleslimit}'
-    requesturl +='&q='+topic
+    newsApiKey = os.getenv("NEWSDATAKEY")
+    requesturl = 'https://newsdata.io/api/1/news?'+'apikey='+newsApiKey+'&language=en'+"&country=au,nz"
+    requesturl +='&from_date='+nfrom
+    requesturl +=f'&category={category}'
+    requesturl += ('&q='+q) if q is not None else ''
 
+    articles = []
     response = requests.get(requesturl)
-    return convertNewsResponseToContent(response)
+    if response.status_code == 200:
+        result = response.json()
+        counter = 0
+        for arr in result['results']:
+            try:
+                c_src = arr['source_id']
+                c_auth = arr['creator'][0] if arr['creator'] is not None else ''
+                c_title = arr['title']
+                c_url = arr['link']
+                c_dsc = arr['description']
+                c_cntnt = arr['content']
+                content = {'source':c_src, 'author':c_auth, 'title':c_title, 'url':c_url, 'description':c_dsc, 'content':c_cntnt}
+                articles.append(content)
+                counter+=1
+            except TypeError as te:
+                print("Error: ",te)
+                #print(arr)
+            if counter>=articleslimit:
+                break
+    else:
+        print(f"Error: {response.status_code}")
+    return articles
 #--------------------------------#
 
 #--------------------------------#
@@ -27,7 +60,7 @@ def getArticleSummary(content, temperature=0.25):
     import os,openai
     openai.api_key = os.getenv("OPENAPIKEY")
     openai.organization = os.getenv("OPENAPIORG")
-    result = """Summarise text below into a short story-line with a two-three emoji (assisting describing content, but not replacing the content):
+    result = """Summarise text below into a short sentense with a two-three emoji at the beginning (assisting describing content, but not replacing the content):
     """ + content[:2048]
     completion = openai.ChatCompletion.create(
         model="gpt-3.5-turbo", messages=[{"role": "user", "content": result}], temperature=temperature
@@ -36,6 +69,15 @@ def getArticleSummary(content, temperature=0.25):
 #--------------------------------#
 
 #--------------------------------#
+def prepareTelegramHTMLmessage(message):
+    #we need this instead of html.escape(message) as Telegram doesn't support all special tags, only some
+    text = message
+    text = text.replace("<","&lt;")
+    text = text.replace(">","&gt;")
+    text = text.replace("&","&amp;")
+    text = text.replace('"',"&quot;")
+    return text
+
 def sendTelegramMessage(message, debug = 1):
     import os,requests
     if debug == 0:
@@ -43,9 +85,13 @@ def sendTelegramMessage(message, debug = 1):
         telegramToken = os.getenv("TELEGRAMBOTKEY")
     else:
         chat_id = "-1001371860541" #TestTelegramChannel
-        telegramToken = os.getenv("TELEGRAMBOTKEYTEST")
-    
-    requesturl = "https://api.telegram.org/bot" + telegramToken + "/sendMessage" + "?chat_id=" + chat_id + "&parse_mode=HTML" + "&text=" + message 
-    requesturl +="&disable_web_page_preview=true"
-    return requests.get(requesturl)
+        telegramToken = os.getenv("TELEGRAMBOTTESTKEY")
+    if telegramToken is None:
+        print("Missing token Key")
+        telegramToken = ""
+
+    return requests.post(
+        url='https://api.telegram.org/bot{0}/{1}'.format(telegramToken, "sendMessage"),
+        data={'chat_id': chat_id, 'disable_web_page_preview': True, 'parse_mode': 'HTML', 'text': message}
+    )
 #--------------------------------#
--- newsGenerator.py
+++ newsGenerator.py
@@ -1,19 +1,21 @@
-from funcLib import getNewsArticles,getArticleSummary,sendTelegramMessage
+from funcLib import getNewsIOArticles,getArticleSummary,sendTelegramMessage,prepareTelegramHTMLmessage
 
 #"Brisbane","Auckland","Economy","Crypto","Industry","Business","Shopping","Lifestyle","Boobs","Aviation","Aircraft","Travel","Technology"
 
 #posts = {"Queensland": 2, "New Zealand": 2, "Aviation":1, "Boobs":1, "IT News": 4, "Science": 3}
-posts = {"Queensland": 2, "New Zealand": 2, "Aviation":2, "Technology": 3, "Science": 3}
+#posts = {"Queensland": 2, "New Zealand": 2, "Aviation":2, "Technology": 3, "Science": 3}
+
+posts = {"Economy":2, "Business":2, "Aviation":1, "Technology": 2, "Science": 2}
 
 #to-do:
 #add headlines
-#save results first, then post (easier to debug)
+#collect news first, then process, then post (easier to debug)
 #use Google Trends to generate list of topics
 
 result = ""
 for topic,numberOfArticles in posts.items():
     print(f"Topic: {topic} ({numberOfArticles})")
-    news = getNewsArticles(topic,numberOfArticles)
+    news = getNewsIOArticles(topic,numberOfArticles)
 
     #Integrated ADs
     #if topic == "New Zealand":
@@ -24,15 +26,27 @@ for topic,numberOfArticles in posts.items():
     if len(news)>0:
         result += "\n<b>" + topic + ":</b>\n"
         for article in news:
-            if len(article['description'])>0:
-                message = "Description: " + article['description'] + "\nContent: " + article['content']
-                articleSummary = getArticleSummary(message)
-                sourceURL = ' (<a href="'+article['url']+'"><i>'+article['source']+'</i></a>)\n'
-                if (len(result) + len(articleSummary) + len(sourceURL)) <= 4096: #Telegram message limit
-                    result += articleSummary + sourceURL
-                else:
-                    print("Message is too long")
-                    break
+            try:
+                title = article['title']
+                description = article['description'] if article['description'] is not None else ''
+                content = article['content'] if article['content'] is not None else description
+                url = article['url'] if article['url'] is not None else ''
+                source = article['source'] if article['source'] is not None else ''
+                message = "Description: " + description + "\nContent: " + content
+                sourceURL = ' (<a href="'+url+'">'+prepareTelegramHTMLmessage(source)+'</a>)\n'
+                if len(description)>0:
+                    articleSummary = getArticleSummary(message)
+                    articleSummary = prepareTelegramHTMLmessage(articleSummary)
+                    if (len(result) + len(articleSummary) + len(sourceURL)) <= 4096: #Telegram message limit
+                        result += articleSummary + sourceURL
+                    else:
+                        print("Message is too long")
+                        break
+            except:
+                print(title)
+                print(description)
+                print(url)
+                print(source)
     print("Articles generated")
 
 if len(result)>0:
@@ -42,4 +56,3 @@ if len(result)>0:
     else:
         print(result)
         print(response.json())
-        
